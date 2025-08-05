0bcd46879d3c33d9a3d2e3b4b87722104541313f
John Seong <wonmor@gmail.com>
2025-08-05 22:38:57 +0100

    Added

---
+++ MusicPlayer/MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by John Seong on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicPlayer/MusicPlayerTests/MusicPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerTests.swift
+//  MusicPlayerTests
+//
+//  Created by John Seong on 8/5/25.
+//
+
+import Testing
+@testable import MusicPlayer
+
+struct MusicPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
